c29ff24fd03702f6ab6ce1fea2cbdb30896fdf8f
dtklumpp <dtklumpp@gmail.com>
2020-08-26 16:29:59 -0400

    made files

---
+++ app.js
@@ -0,0 +1,29 @@
+const logit = function(input1){
+    console.log(input1);
+}
+logit('you are hearing me talk');
+
+//round:
+//you attack 1st alien ship
+//if survives, attacks you
+//then etc
+//if destroy, can attack or retreat
+//if retreat, game over (draw)
+//if destroy all, win
+//if killed, lose
+//
+//hull = hp
+//firepower = damage done with hit
+//accuracy = prob of hit
+//
+//your shit USS Assembly
+//hull 20
+//firepower 5
+//accuracy .7
+//aliens
+//hull 3-6 randomly
+//firepower 2-4 randomly
+//accuracy .6-.8 randomly
+
+//battle 6 ships unique values
+
